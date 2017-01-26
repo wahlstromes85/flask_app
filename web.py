ab9a54826c47873317ee1a57d0fc6c8564b24f15
Eric Wahlstrom <eric@eric-mbp.local>
2017-01-25 16:31:24 -0800

    First commit

---
+++ web.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import weather
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	# name = request.values.get('name')
+	address = request.values.get('address')
+	forecast = None
+	if address:
+		forecast = weather.get_weather(address)
+	return render_template('index.html',
+		forecast=forecast) 
+
+@app.route("/about")
+def about():
+	return render_template('about.html')
+
+if __name__ == "__main__":
+	app.run()
